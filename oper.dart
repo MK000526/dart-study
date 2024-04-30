fa96d5f0a9f171c8b14589920fa417527fcf9e5f
mk000526 <skzhffk97@naver.com>
2024-04-30 19:25:54 +0900

    oper

---
+++ oper.dart
@@ -0,0 +1,28 @@
+void main() {
+  int a = 1;
+  int b = 1;
+  // print(a >= b);
+
+  // print(a);
+
+  // a = a + 3;
+  // print(a);
+
+  // a++;
+  // print(a);
+  // ++a;
+  // print(a);
+
+  // a = a * 3;
+  // a += 3;
+  // a++;
+
+  int c = 3;
+  // print(c++);
+  // print(c);
+  // print(++c);
+
+  print(c--);
+  print(c);
+  print(--c);
+}
