335b8b89be5d5122034e137a748fac29cb66f95f
mk000526 <skzhffk97@naver.com>
2024-04-30 18:37:28 +0900

    variable

---
+++ variable.dart
@@ -0,0 +1,18 @@
+void main() {
+  var number = 10;
+  print(number);
+  number = 20;
+  print(number);
+
+// ------------------------------------
+
+  print(" --------------");
+  dynamic number2 = 10;
+  print(number2);
+  number2 = 20;
+  print(number2);
+  number2 = "hello World";
+  print(number2);
+  number2 = 1.1;
+  print(number2);
+}
